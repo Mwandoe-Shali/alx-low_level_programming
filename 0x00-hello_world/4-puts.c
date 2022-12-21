81dcca82e179cd9b1365ad584e20e252f77875b3
Mwandoe-Shali <mwandoeshali@gmail.com>
2022-12-21 12:51:05 -0800

    happy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - prints "Programming is like building a multilingual puzzle
  *
- *  * main - prints "Programming is like building a multilingual puzzle"
- *
- *   *
- *
- *    * Return: Always 0 (success)
- *
- *     */
+ * Return: Always 0
+ */
 
 int main(void)
 
+
 {
-
-	    puts("Programming is like building a multilingual puzzle");
-
-	        return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
-
-
