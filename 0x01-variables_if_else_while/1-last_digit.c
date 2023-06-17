36528261ddc0e1308f2eb46c61dfb6e3a3c6e825
Mwandoe-Shali <mwandoeshali@gmail.com>
2023-06-17 14:54:52 -0700

    mostest errors

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,24 +7,28 @@
  * number stored in the variable n
  * Return: Always 0 (Success)
  */
-int main(void) {
-  int n;
-  int last_digit;
+int main(void)
+{
+	int n;
+       	int last_digit;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
+  	srand(time(0));
+  	n = rand() - RAND_MAX / 2;
 
-  last_digit = n % 10;
-  printf("Last digit of %d is %d\n", n, last_digit);
-
-  if (last_digit > 5) {
-    printf("and is greater than 5\n");
-  } else if (last_digit == 0) {
-    printf("and is 0\n");
-  } else {
-    printf("and is less than 6 and not 0\n");
-  }
-
-  return 0;
+      	last_digit = n % 10;
+  	printf("Last digit of %d is %d\n", n, last_digit);
+	if (last_digit > 5)
+       	{
+	       	printf("and is greater than 5\n");
+	}
+       	else if (last_digit == 0)
+       	{
+		printf("and is 0\n");
+       	}
+       	else
+       	{
+	       	printf("and is less than 6 and not 0\n");
+	}
+      	return 0;
 }
 
