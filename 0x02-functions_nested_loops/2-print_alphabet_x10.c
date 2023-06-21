3621831a9f5ce20d8db73b7c8e83d897305a9572
Mwandoe-Shali <mwandoeshali@gmail.com>
2023-06-21 06:53:10 -0700

    prints alphabet in lowercase using _putchar twice

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet in lowercase, ten times
- */
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+* followed by a new line
+*/
 void print_alphabet_x10(void)
 {
 char letter;
 int count;
 
-for (count = 0; count < 10; count++)
+count = 0;
+
+while (count < 10)
 {
-for (letter = 'a'; letter <= 'z'; letter++)
+letter = 'a';
+while (letter <= 'z')
 {
 _putchar(letter);
-}
+letter++;
 }
 _putchar('\n');
+count++;
+}
 }
 
