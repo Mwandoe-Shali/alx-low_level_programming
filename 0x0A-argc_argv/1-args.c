ea70b173ba746be5a7a1684b83c1fa453eef0f6d
Mwandoe-Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-07-10 14:43:19 +0300

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0
+ * 
+ * @argc: Counts the number of arguments in the command line arguments
+ * @argv: Array of the arguments
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
