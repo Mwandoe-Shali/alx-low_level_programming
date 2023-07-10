e19856972c5f76c3d8706efde4f67e8afb2dfca8
Mwandoe-Shali <mwandoeshali@gmail.com>
2023-07-10 14:21:27 -0700

    Write a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,10 +7,11 @@
  * @argc: Counts the number of arguments in the command line arguments
  * @argv: Array of the arguments
  */
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
 	(void)argv;
 	
 	printf("%d\n", argc - 1);
 	return (0);
 }
+
