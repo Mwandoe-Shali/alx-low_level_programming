ad15806128d8a0f5301a6e861d122cf0ea1dffa5
Mwandoe-Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-07-12 22:54:48 +0300

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,16 +2,16 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * free_grid - allocates a grid, make space and free space
+ * @grid: grid to free
+ * @height: height of the array
+ * Return:returns void
  */
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL || height = 0)
+	if (grid == NULL || height == 0)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
