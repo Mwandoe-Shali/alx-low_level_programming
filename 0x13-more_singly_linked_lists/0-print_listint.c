6af7492bed3e6b5989956854cd8ebcfcc692d1ba
Mwandoe-Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-07-31 22:44:38 +0300

    Write a function that prints all the elements of a listint_t list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,25 +8,25 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	/* 
- 	 * Declare a pointer variable 'current_node'
- 	 * and initialize it to the head of the list
-   	 */
+	/*
+	* Declare a pointer variable 'current_node'
+	* and initialize it to the head of the list
+	*/
 	const listint_t *current_node = h;
 	/* Variable to keep track of the number of nodes */
 	size_t count = 0;
 
-    	/* Traverse the list while there are nodes (h != NULL) */
-    	while (current_node != NULL)
-    	{
-        	/* Print the value of the current node */
+	/* Traverse the list while there are nodes (h != NULL) */
+	while (current_node != NULL)
+	{
+		/* Print the value of the current node */
 		printf("%d\n", current_node->n);
 		/* Move to the next node */
-        	current_node = current_node->next;
+		current_node = current_node->next;
 		/* Increment the count of nodes */
-        	count++;
-    	}
+		count++;
+	}
 
-    	/* Return the total number of nodes in the list */
+	/* Return the total number of nodes in the list */
 	return (count);
 }
