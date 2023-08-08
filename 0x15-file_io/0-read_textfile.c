27faf9602dcb198192874525a3becb51e8ae0908
Mwandoe-Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-08-08 13:03:28 +0300

    Write a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,8 @@
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  *
- * Return: The actual number of letters read and printed, or 0 if an error occurs.
+ * Return: The actual number of letters read
+ * and printed, or 0 if an error occurs.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
