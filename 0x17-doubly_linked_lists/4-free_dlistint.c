10a9b06f392b059d17c3c2ecaf2b4585796f5329
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-17 17:07:10 +0300

    Create 4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a DLL
+ * @head: Head of DLL
+ *
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+	dlistint_t *next;
+
+	if (head == NULL)
+		return;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
+		
