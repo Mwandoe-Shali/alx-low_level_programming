d841360106fb64b793a7d8c2697dd2935ff69dff
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-17 17:32:33 +0300

    Create 5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+/**
+ * get_dnodeint_at_index - returns the nth node of DLL
+ *
+ * @head: head of the DLL
+ * @index: index of the nth node
+ * Return: nth node
+ * 		if the node does not exist, return NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int n;
+	
+	current = head;
+	for (n = 0; n < index; n++)
+	{
+        	if (current == NULL)
+            		return (NULL);
+
+        	current = current->next;
+	}
+
+	return (current);
+}
+		
