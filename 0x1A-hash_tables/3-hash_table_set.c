88c830f304278d8befbaf58bf02b5f7eb474e8d5
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-10-09 13:20:07 +0300

    Create 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,46 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: The hash table
+ * @key: The key
+ * @value: The value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node, *temp;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return 0;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	/* Check if the key already exists in the hash table */
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			/* Update the value associated with the existing key */
+			free(temp->value);
+			temp->value = strdup(value);
+			return 1;
+		}
+		temp = temp->next;
+	}
+
+	/* Create a new node */
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return 0;
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return 1;
+}
