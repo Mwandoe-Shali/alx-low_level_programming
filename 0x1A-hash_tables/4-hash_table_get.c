273819fb181cef243e03d3c7c078b212c728f5b4
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-10-09 13:28:21 +0300

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,7 +5,7 @@
  * @ht: The hash table
  * @key: The key to search for
  *
- * Return: The value associated with the key, or 
+ * Return: The value associated with the key, or
  *		NULL if key couldn't be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
