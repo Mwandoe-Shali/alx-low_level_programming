859d3fdcf61c871ae699d1755f6b821232e64e2d
Mwandoe-Shali <mwandoeshali@gmail.com>
2024-04-29 20:14:16 +0300

    Let's see if it works

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,44 +1,37 @@
 #include "search_algos.h"
-#include <math.h>
 
 /**
- * jump_search - searches for a value in an array of
- * integers using the Jump search algorithm
+ * jump_search - function that searches for a value
+ * in a sorted array of integers
  *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * @array: a pointer to the first element of the array to search in
+ * @size:  the number of elements in array
+ * @value: is the value to search for
+ * Return: the first index where value is located
  */
+
 int jump_search(int *array, size_t size, int value)
 {
-	int idx, a, b, prev;
+	size_t a, stair, ruka;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	a = (int)sqrt((double)size);
-	b = 0;
-	prev = idx = 0;
+	stair = sqrt(size);
 
-	do {
-		printf("Value checked array[%d] = [%d]\n", idx, array[idx]);
-
-		if (array[idx] == value)
-			return (idx);
-		b++;
-		prev = idx;
-		idx = b * a;
-	} while (idx < (int)size && array[idx] < value);
-
-	printf("Value found between idxes [%d] and [%d]\n", prev, idx);
-
-	for (; prev <= idx && prev < (int)size; prev++)
+	for (a = ruka = 0; ruka < size && array[ruka] < value;)
 	{
-		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-		if (array[prev] == value)
-			return (prev);
+		printf("Value checked array [%ld] = [%d]\n", ruka, array[ruka]);
+		a = ruka;
+		ruka += stair;
 	}
 
-	return (-1);
+	printf("Value found between indexes [%ld] and [%ld]\n", a, ruka);
+
+	ruka = ruka > size - 1 ? ruka : size - 1;
+	for (; a < ruka && array[a] < value; a++)
+		printf("Value checked array [%ld] = [%d]\n", a, array[a]);
+	printf("Value checked array [%ld] = [%d]\n", a, array[a]);
+
+	return (array[a] == value ? (int)a : -1);
 }
